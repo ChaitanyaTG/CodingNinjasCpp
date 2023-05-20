c92952d9c3314aa213396d2aa43140ba8c2fbc3b
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-05-20 20:52:11 +0530

    Add files via upload

---
+++ FindAreaOfRectangle.cpp
@@ -0,0 +1,19 @@
+//
+//  FindAreaOfRectangle.cpp
+//  CodingNinjasCpp
+//
+//  Created by Chaitanya Gadkari on 19/05/23.
+//
+
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int l, b;
+    cin >> l >> b; // Taking length and breadth as input
+    int area;
+    area = l * b; //Calculating area
+    cout << area; //Area as output
+    return 0;
+}
