3bcae69c262d94ca63a7c4846bdae1e44d2c6856
ChaitanyaTG <70212909+ChaitanyaTG@users.noreply.github.com>
2023-05-20 20:51:57 +0530

    Add files via upload

---
+++ PrintName&Age.cpp
@@ -0,0 +1,18 @@
+//
+//  PrintName&Age.cpp
+//  CodingNinjasCpp
+//
+//  Created by Chaitanya Gadkari on 19/05/23.
+//
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    cin >> name >> age; // Taking name and age as input
+    cout << "The name of the person is" <<name<< "and the age is "<<age<<".";
+    return 0;
+}
